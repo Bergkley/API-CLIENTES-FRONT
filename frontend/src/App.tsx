a2268e9acfd1e8917e1e964475ff234d4668d42c
Bergkley <bergkley@gmail.com>
2024-07-24 22:26:11 -0300

    feat: refatoração

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,54 +1,51 @@
-import {FiTrash} from 'react-icons/fi';
+import { FiTrash } from "react-icons/fi";
 
-export  default function App() {
+export default function App() {
   return (
     <div className="w-full min-h-screen bg-gray-900 flex justify-center px-4">
-        <main className="my-10 w-full md:max-w-2xl">
-          <h1 className="text-4xl font-medium text-white">Clientes</h1>
+      <main className="my-10 w-full md:max-w-2xl">
+        <h1 className="text-4xl font-medium text-white">Clientes</h1>
 
-          <form className="flex flex-col my-6 ">
-            <label className="font-medium text-white">Nome:</label>
-            <input 
+        <form className="flex flex-col my-6 ">
+          <label className="font-medium text-white">Nome:</label>
+          <input
             type="text"
             placeholder="Digite seu nome completo ..."
             className="w-full mb-5 p-2 rounded"
-             />
+          />
 
           <label className="font-medium text-white">Email:</label>
-            <input 
+          <input
             type="email"
             placeholder="Digite seu nome email ..."
             className="w-full mb-5 p-2 rounded"
-             />
+          />
 
-             <input 
-             type="submit"
-             value="Cadastrar"
-             className="cursor-pointer w-full p-2 bg-green-500 rounded font-medium"              
-              />
+          <input
+            type="submit"
+            value="Cadastrar"
+            className="cursor-pointer w-full p-2 bg-green-500 rounded font-medium"
+          />
 
+          <section className="flex flex-col">
+            <article className="w-full bg-white rounded p-2 mt-5 relative hover:scale-105 duration-200">
+              <p>
+                <span className="font-medium">Nome:</span> Berg
+              </p>
+              <p>
+                <span className="font-medium">Email:</span> bergkley@gmail.com
+              </p>
+              <p>
+                <span className="font-medium">Status:</span> ativo
+              </p>
 
-              <section className="flex flex-col">
-
-                <article 
-                className="w-full bg-white rounded p-2 mt-5 relative hover:scale-105 duration-200"
-                >
-                  <p><span className="font-medium">Nome:</span> Berg</p>
-                  <p><span className="font-medium">Email:</span> bergkley@gmail.com</p>
-                  <p><span className="font-medium">Status:</span> ativo</p>
-
-                  <button className='bg-red-500 w-7 h-7 flex items-center justify-center rounded-lg absolute right-0 -top-2'>
-                    <FiTrash 
-                    size={18}
-                    color="#fff"
-                    />
-                  </button>
-                  
-                </article>
-
-              </section>
-          </form>
-        </main>
+              <button className="bg-red-500 w-7 h-7 flex items-center justify-center rounded-lg absolute right-0 -top-2">
+                <FiTrash size={18} color="#fff" />
+              </button>
+            </article>
+          </section>
+        </form>
+      </main>
     </div>
-  )
+  );
 }
