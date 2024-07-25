642afce7bd8b9a06b10209edf6892a9ae7589c43
Bergkley <bergkley@gmail.com>
2024-07-24 22:17:24 -0300

    feat: criado div principal

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 export  default function App() {
   return (
-    <div>
-      <h1>App</h1>
+    <div className="w-full min-h-screen bg-gray-900 flex justify-center px-4">
+        
     </div>
   )
 }
